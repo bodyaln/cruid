27174d58c27c7b72b762f3a0af38a0790c659471
bodyaln <jokerlinnik@gmail.com>
2023-10-17 21:27:23 +0200

    first commit

---
+++ src/components/app-info/app-info.jsx
@@ -0,0 +1,12 @@
+import './app-info.css'
+function AppInfo() {
+  return (
+    <div className="app-info">
+      <h1>Учет сотрудников</h1>
+      <h2>Общее число сотрудников</h2>
+      <h2>Получат премию: </h2>
+    </div>
+  );
+}
+
+export default AppInfo;
